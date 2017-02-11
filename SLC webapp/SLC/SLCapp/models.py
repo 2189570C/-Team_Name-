22e4f394145ebc8d5577d6c835bb888729e6e6e2
Iain Cattermole <2189570c@student.gla.ac.uk>
2017-02-11 15:57:48 +0000

    More user functionality

--- SLC webapp/SLC/SLCapp/models.py
+++ SLC webapp/SLC/SLCapp/models.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-
-    def __str__(self):
-        return self.user.username
-    
-    def __unicode__(self):
-        return self.user.username
+##class UserProfile(models.Model):
+##    user = models.OneToOneField(User)
+##
+##    def __str__(self):
+##        return self.user.username
+##    
+##    def __unicode__(self):
+##        return self.user.username
--- SLC webapp/SLC/SLCapp/urls.py
+++ SLC webapp/SLC/SLCapp/urls.py
@@ -3,4 +3,7 @@ from SLCapp import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout')
+    url(r'^signup/$', views.signup, name='signup')
 ]
