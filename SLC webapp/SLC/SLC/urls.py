4cfabd2bcaa21de16bcd97bef21bc996805c0bac
Iain Cattermole <2189570c@student.gla.ac.uk>
2017-02-11 14:41:15 +0000

    Start of app

--- SLC webapp/SLC/SLC/urls.py
+++ SLC webapp/SLC/SLC/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^SLCapp/$', include('SLCapp.urls')),
+    url(r'^$', views.home, name='home'),
 ]
