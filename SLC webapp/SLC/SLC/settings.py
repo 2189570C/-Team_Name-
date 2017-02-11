02d5b8a2de430f6ba7c749005357f3a82b729485
Iain Cattermole <2189570c@student.gla.ac.uk>
2017-02-11 14:54:30 +0000

    updated settings

--- SLC webapp/SLC/SLC/settings.py
+++ SLC webapp/SLC/SLC/settings.py
@@ -30,6 +30,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+
+# Media files
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
