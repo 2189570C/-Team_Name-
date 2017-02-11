e8b799d6377b0137c78fa8cb65d1b7edb6cbe0b3
Iain Cattermole <2189570c@student.gla.ac.uk>
2017-02-11 14:58:49 +0000

    home finished

--- SLC webapp/SLC/SLC/urls.py
+++ SLC webapp/SLC/SLC/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from SLCapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
